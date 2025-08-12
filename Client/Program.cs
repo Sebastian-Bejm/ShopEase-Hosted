696321e456b5d90169ece67db41ec73fabfa0f01
Sebastian Bejm <125sbejm@gmail.com>
2025-08-12 16:57:56 -0700

    migrated files from old wasm project

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ShopEase.Shared.Services;
 
 namespace ShopEase.Client
 {
@@ -18,6 +19,7 @@ namespace ShopEase.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<CartService>();
 
             await builder.Build().RunAsync();
         }
---
+++ Shared/Services/CartService.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Linq;
+using ShopEase.Shared.Models;
+
+namespace ShopEase.Shared.Services
+{
+    public class CartService {
+        private Dictionary<int, (Product Product, int Quantity)> cartItems = new();
+
+        public IEnumerable<(int ProductID, Product Product, int Quantity)> CartItems =>
+        cartItems.Select(kvp => (kvp.Key, kvp.Value.Product, kvp.Value.Quantity));
+
+        public void AddProduct(Product product)
+        {
+            if (cartItems.ContainsKey(product.ProductID))
+            {
+                var (existingProduct, quantity) = cartItems[product.ProductID];
+                cartItems[product.ProductID] = (existingProduct, quantity + 1);
+            }
+            else
+            {
+                cartItems[product.ProductID] = (product, 1);
+            }
+        }
+
+        public void RemoveProduct(int productId) {
+            if (cartItems.ContainsKey(productId))
+            {
+                var (product, quantity) = cartItems[productId];
+                if (quantity > 1)
+                {
+                    cartItems[productId] = (product, quantity - 1);
+                }
+                else
+                {
+                    cartItems.Remove(productId);
+                }
+            }
+        }
+
+        public void SetProductQuantity(int productId, int quantity)
+        {
+            if (cartItems.ContainsKey(productId))
+            {
+                var existing = cartItems[productId].Product;
+                if (quantity <= 0)
+                {
+                    cartItems.Remove(productId);
+                }
+                else
+                {
+                    cartItems[productId] = (existing, quantity);
+                }
+            }
+        }
+
+
+        public decimal CalculateTotal()
+        {
+            return cartItems.Values.Sum(item => item.Product.Price * item.Quantity);
+        }
+    }
+}
